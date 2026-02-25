9d2c643ebd7ce981b8f3337efe59131af93144c3
Gustavo Morinaga <me@gustavomorinaga.dev>
2026-02-25 01:00:12 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { asset } from '$app/paths';
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={asset('/assets/favicon.svg')} />
+</svelte:head>
+
+{@render children()}
